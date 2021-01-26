92f0102c595ef349725a515cd9e96c77c6379ec0
sophiemeakin <sophiemeakin@outlook.com>
2021-01-26 17:49:35 +0000

    Add Trust-level case data to update_admissions_forecasts

--- forecasts/update_admissions_forecasts.R
+++ forecasts/update_admissions_forecasts.R
@@ -20,9 +20,19 @@ raw_case <- covidregionaldata::get_regional_data("UK", include_level_2_regions =
 
 # Reshape data ------------------------------------------------------------
 
+case <- raw_case %>%
+  dplyr::filter(date >= min(raw_adm$date)) %>%
+  dplyr::mutate(utla_code = ifelse(utla_code == "E10000002", "E06000060", utla_code)) %>%
+  dplyr::left_join(covid19.nhs.data::trust_utla_mapping, by = c("utla_code" = "geo_code")) %>%
+  dplyr::mutate(trust_new_case = p_geo * cases_new) %>%
+  dplyr::group_by(id = trust_code, date) %>%
+  dplyr::summarise(new_case = round(sum(trust_new_case, na.rm = TRUE)),
+                   .groups = "drop")
+
 adm <- raw_adm %>%
   dplyr::filter(trust_code %in% covid19.nhs.data::trust_ltla_mapping$trust_code) %>%
-  dplyr::select(id = trust_code, date, new_adm = admissions) 
+  dplyr::select(id = trust_code, date, new_adm = admissions) %>%
+  dplyr::left_join(case, by = c("id", "date"))
 
 
 
