54c25c57774c7b181ea7ff541ec5d35f8ff641a6
sophiemeakin <sophiemeakin@outlook.com>
2021-03-05 15:50:03 +0000

    Add convolution model with observed data

--- forecasts/forecast_convolution_admissions.R
+++ forecasts/forecast_convolution_admissions.R
@@ -10,14 +10,52 @@ for(forecast_date in forecast_dates){
   
   # Reshape observed data
   df_observed <- offline_hosp %>%
-    dplyr::filter(date >= forecast_date - 42,
+    dplyr::filter(date >= forecast_date - (42+21),
                   date <= forecast_date) %>%
     dplyr::select(region = id, date, primary = new_case, secondary = all_adm) %>%
     na.omit()
   dt_observed <- data.table::data.table(df_observed)
   
   
-  # Time series ensemble forecast
+  # Observed data
+  df_forecast_in <- offline_hosp %>%
+    dplyr::filter(date > forecast_date,
+                  date <= forecast_date + 14) %>%
+    dplyr::select(region = id, date, cases = new_case)
+  df_forecast <- purrr::map_df(.x = 1:1000, .f = ~ {
+    out <- df_forecast_in %>%
+      dplyr::mutate(sample = as.integer(.x))
+    return(out)
+  }) %>%
+    dplyr::bind_rows()
+  dt_forecast <- data.table::data.table(df_forecast)
+  
+  ## Run regional_secondary with observed cases
+  convolution_forecast_observed <- regional_secondary(reports = dt_observed %>% filter(region == "R0A"),
+                                                      case_forecast = dt_forecast %>% filter(region == "R0A"),
+                                                      secondary = secondary_opts(type = "incidence"),
+                                                      obs = EpiNow2::obs_opts(week_effect = FALSE,
+                                                                              scale = list(mean = 0.2, sd = 0.1)),
+                                                      burn_in = 21,
+                                                      control = list(adapt_delta = 0.99, max_treedepth = 15),
+                                                      return_fit = FALSE,
+                                                      return_plots = FALSE,
+                                                      verbose = TRUE)
+  
+  ## Reshape samples and summary
+  convolution_samples_observed <- convolution_forecast_observed$samples %>%
+    dplyr::mutate(value = round(value)) %>%
+    dplyr::filter(date > forecast_date) %>%
+    dplyr::mutate(forecast_from = forecast_date,
+                  horizon = as.integer(date - forecast_from),
+                  model = "convolution_observed") %>%
+    dplyr::select(id = region, sample, horizon, value, forecast_from, model)
+  
+  convolution_summary_observed <- forecast_summary(samples = convolution_samples_observed)
+  
+  
+  
+  # Rt case forecast
   case_forecast_file <- paste0("epinow_utla_", forecast_date, ".rds")
   case_forecast_summary <- readRDS(file = here::here("data", "utla_case_forecast", case_forecast_file))
   case_forecast_samples <- epinow_samples(df = case_forecast_summary)
@@ -35,32 +73,37 @@ for(forecast_date in forecast_dates){
   dt_forecast <- data.table::data.table(df_forecast)
   
   ## Run regional_secondary with EpiNow2 UTLA case forecast
-  secondary_forecast <- regional_secondary(reports = dt_observed,
+  convolution_forecast_rt <- regional_secondary(reports = dt_observed,
                                            case_forecast = dt_forecast,
-                                           return_fit = FALSE,
-                                           return_plots = FALSE,
                                            secondary = secondary_opts(type = "incidence"),
                                            obs = EpiNow2::obs_opts(week_effect = FALSE,
                                                                    scale = list(mean = 0.2, sd = 0.1)),
+                                           burn_in = 21,
                                            control = list(adapt_delta = 0.99, max_treedepth = 15),
+                                           return_fit = FALSE,
+                                           return_plots = FALSE,
                                            verbose = TRUE)
   
   ## Reshape samples and summary
-  secondary_samples <- secondary_forecast$samples %>%
+  convolution_samples_rt <- convolution_forecast_rt$samples %>%
     dplyr::mutate(value = round(value)) %>%
     dplyr::filter(date > forecast_date) %>%
     dplyr::mutate(forecast_from = forecast_date,
                   horizon = as.integer(date - forecast_from),
-                  model = "epinow2_secondary") %>%
+                  model = "convolution_rt") %>%
     dplyr::select(id = region, sample, horizon, value, forecast_from, model)
   
-  secondary_summary <- forecast_summary(samples = secondary_samples)
+  convolution_summary_rt <- forecast_summary(samples = convolution_samples_rt)
   
   
   # Save results
-  forecast_name <- paste0("admissions_secondary_", forecast_date, ".rds")
-  saveRDS(object = secondary_samples, file = here::here("forecasts", "samples", forecast_name))
-  saveRDS(object = secondary_summary, file = here::here("forecasts", "summary", forecast_name))
+  convolution_samples <- convolution_samples_observed %>%
+    dplyr::bind_rows(convolution_samples_rt)
+  convolution_summary <- convolution_summary_observed %>%
+    dplyr::bind_rows(convolution_summary_rt)
+  forecast_name <- paste0("admissions_convolution_", forecast_date, ".rds")
+  saveRDS(object = convolution_samples, file = here::here("forecasts", "samples", forecast_name))
+  saveRDS(object = convolution_summary, file = here::here("forecasts", "summary", forecast_name))
   
   
 }
--- forecasts/forecast_convolution_occupancy.R
+++ forecasts/forecast_convolution_occupancy.R
@@ -2,17 +2,58 @@
 devtools::source_gist("https://gist.github.com/seabbs/4dad3958ca8d83daca8f02b143d152e6")
 samples_dir <- here::here("forecasts", "samples")
 
+offline_hosp <- readRDS(file = here::here("data", "raw", "offline_hosp.rds"))
+offline_mapping <- readRDS(file = here::here("data", "raw", "offline_mapping.rds"))
+
 for(forecast_date in forecast_dates){
   
   forecast_date <- as.Date(forecast_date)
   
   # Reshape observed data
-  df_observed <- hosp %>%
-    dplyr::filter(date >= forecast_date - 42,
+  df_observed <- offline_hosp %>%
+    dplyr::filter(date >= forecast_date - (21 + 42),
                   date <= forecast_date) %>%
     dplyr::select(region = id, date, primary = all_adm, secondary = bed_occ) %>%
     na.omit()
   dt_observed <- data.table::data.table(df_observed)
+  
+  
+  # Observed data
+  df_forecast_in <- offline_hosp %>%
+    dplyr::filter(date > forecast_date,
+                  date <= forecast_date + 14) %>%
+    dplyr::select(region = id, date, cases = all_adm)
+  df_forecast <- purrr::map_df(.x = 1:1000, .f = ~ {
+    out <- df_forecast_in %>%
+      dplyr::mutate(sample = as.integer(.x))
+    return(out)
+  }) %>%
+    dplyr::bind_rows()
+  dt_forecast <- data.table::data.table(df_forecast)
+  
+  ## Run regional_secondary with observed cases
+  convolution_forecast_observed <- regional_secondary(reports = dt_observed,
+                                                      case_forecast = dt_forecast,
+                                                      delays = delay_opts(list(mean = 2.5, mean_sd = 0.4, 
+                                                                               sd = 0.47, sd_sd = 0.2, max = 30)),
+                                                      secondary = secondary_opts(type = "prevalence"),
+                                                      obs = EpiNow2::obs_opts(week_effect = FALSE),
+                                                      burn_in = 21,
+                                                      control = list(adapt_delta = 0.99, max_treedepth = 15),
+                                                      return_fit = FALSE,
+                                                      return_plots = FALSE,
+                                                      verbose = TRUE)
+  
+  ## Reshape samples and summary
+  convolution_samples_observed <- convolution_forecast_observed$samples %>%
+    dplyr::mutate(value = round(value)) %>%
+    dplyr::filter(date > forecast_date) %>%
+    dplyr::mutate(forecast_from = forecast_date,
+                  horizon = as.integer(date - forecast_from),
+                  model = "convolution_observed") %>%
+    dplyr::select(id = region, sample, horizon, value, forecast_from, model)
+  
+  convolution_summary_observed <- forecast_summary(samples = convolution_samples_observed)
   
   
   # Time series ensemble forecast
@@ -25,25 +66,28 @@ for(forecast_date in forecast_dates){
   dt_forecast_ensemble <- data.table::data.table(df_forecast_ensemble)
   
   ## Run regional_secondary with ensemble admissions forecast
-  secondary_ensemble_forecast <- regional_secondary(reports = dt_observed,
-                                                    case_forecast = dt_forecast_ensemble,
-                                                    return_fit = FALSE,
-                                                    return_plots = FALSE,
-                                                    secondary = secondary_opts(type = "prevalence"),
-                                                    obs = EpiNow2::obs_opts(week_effect = FALSE),
-                                                    control = list(adapt_delta = 0.99, max_treedepth = 15),
-                                                    verbose = TRUE)
+  convolution_forecast_ensemble <- regional_secondary(reports = dt_observed,
+                                                      case_forecast = dt_forecast_ensemble,
+                                                      delays = delay_opts(list(mean = 2.5, mean_sd = 0.4, 
+                                                                               sd = 0.47, sd_sd = 0.2, max = 30)),
+                                                      secondary = secondary_opts(type = "prevalence"),
+                                                      obs = EpiNow2::obs_opts(week_effect = FALSE),
+                                                      burn_in = 21,
+                                                      control = list(adapt_delta = 0.99, max_treedepth = 15),
+                                                      return_fit = FALSE,
+                                                      return_plots = FALSE,
+                                                      verbose = TRUE)
   
   ## Reshape samples and summary
-  secondary_ensemble_samples <- secondary_ensemble_forecast$samples %>%
+  convolution_samples_ensemble <- convolution_forecast_ensemble$samples %>%
     dplyr::mutate(value = round(value)) %>%
     dplyr::filter(date > forecast_date) %>%
     dplyr::mutate(forecast_from = forecast_date,
                   horizon = as.integer(date - forecast_from),
-                  model = "epinow2_secondary_ensemble") %>%
+                  model = "convolution_ensemble") %>%
     dplyr::select(id = region, sample, horizon, value, forecast_from, model)
   
-  secondary_ensemble_summary <- forecast_summary(samples = secondary_ensemble_samples)
+  convolution_summary_ensemble <- forecast_summary(samples = convolution_samples_ensemble)
   
   
   # ARIMA + cases forecast
@@ -57,36 +101,41 @@ for(forecast_date in forecast_dates){
   dt_forecast_arima <- data.table::data.table(df_forecast_arima)
   
   ## Run regional_secondary with ensemble admissions forecast
-  secondary_arima_forecast <- regional_secondary(reports = dt_observed,
-                                                 case_forecast = dt_forecast_arima,
-                                                 return_fit = FALSE,
-                                                 return_plots = FALSE,
-                                                 secondary = secondary_opts(type = "prevalence"),
-                                                 obs = EpiNow2::obs_opts(week_effect = FALSE),
-                                                 control = list(adapt_delta = 0.99, max_treedepth = 15),
-                                                 verbose = TRUE)
+  convolution_forecast_arima <- regional_secondary(reports = dt_observed,
+                                                   case_forecast = dt_forecast_arima,
+                                                   delays = delay_opts(list(mean = 2.5, mean_sd = 0.4, 
+                                                                            sd = 0.47, sd_sd = 0.2, max = 30)),
+                                                   secondary = secondary_opts(type = "prevalence"),
+                                                   obs = EpiNow2::obs_opts(week_effect = FALSE),
+                                                   burn_in = 21,
+                                                   control = list(adapt_delta = 0.99, max_treedepth = 15),
+                                                   return_fit = FALSE,
+                                                   return_plots = FALSE,
+                                                   verbose = TRUE)
   
   ## Reshape samples and summary
-  secondary_arima_samples <- secondary_arima_forecast$samples %>%
+  convolution_samples_arima <- convolution_forecast_arima$samples %>%
     dplyr::mutate(value = round(value)) %>%
     dplyr::filter(date > forecast_date) %>%
     dplyr::mutate(forecast_from = forecast_date,
                   horizon = as.integer(date - forecast_from),
-                  model = "epinow2_secondary_arima") %>%
+                  model = "convolution_arima") %>%
     dplyr::select(id = region, sample, horizon, value, forecast_from, model)
   
-  secondary_arima_summary <- forecast_summary(samples = secondary_arima_samples)
+  convolution_summary_arima <- forecast_summary(samples = convolution_samples_arima)
   
    
   
   # Save results
-  secondary_samples <- secondary_ensemble_samples %>%
-    dplyr::bind_rows(secondary_arima_samples)
-  secondary_summary <- secondary_ensemble_summary %>%
-    dplyr::bind_rows(secondary_arima_summary)
-  forecast_name <- paste0("occupancy_secondary_", forecast_date, ".rds")
-  saveRDS(object = secondary_samples, file = here::here("forecasts", "samples", forecast_name))
-  saveRDS(object = secondary_summary, file = here::here("forecasts", "summary", forecast_name))
+  convolution_samples <- convolution_samples_observed %>%
+    dplyr::bind_rows(convolution_samples_ensemble) %>%
+    dplyr::bind_rows(convolution_samples_arima)
+  convolution_summary <- convolution_summary_observed %>%
+    dplyr::bind_rows(convolution_summary_ensemble) %>%
+    dplyr::bind_rows(convolution_summary_arima)
+  forecast_name <- paste0("occupancy_convolution_", forecast_date, ".rds")
+  saveRDS(object = convolution_samples, file = here::here("forecasts", "samples", forecast_name))
+  saveRDS(object = convolution_summary, file = here::here("forecasts", "summary", forecast_name))
   
   
   
