b31a799f40b9a316b84c48b423ddb627c5383a74
sophiemeakin <sophiemeakin@outlook.com>
2021-03-22 11:35:04 +0000

    Functions to plot forecasts

--- R/plot_forecasts.R
+++ R/plot_forecasts.R
@@ -1,20 +1,59 @@
 
+# Plot observed data ------------------------------------------------------
+
+plot_observed <- function(observed,
+                          trusts = "REM",
+                          scale_date_breaks = "6 weeks",
+                          plot_vars = NULL,
+                          plot_raw = FALSE){
+  
+  if(is.null(plot_vars)){
+    
+    plot_vars <- unique(observed$name)
+    
+  }
+  
+  dat_in <- observed %>%
+    dplyr::filter(id %in% trusts,
+                  name %in% plot_vars) %>% 
+    dplyr::group_by(id, name) %>%
+    dplyr::mutate(value_trend = (stl(ts(value, freq = 7), s.window = 7)$time.series)[,2],
+                  value_std = value/sd(value, na.rm = TRUE),
+                  value_trend_std = (stl(ts(value_std, freq = 7), s.window = 7)$time.series)[,2]) %>%
+    dplyr::ungroup() %>%
+    dplyr::mutate(name = ordered(name, plot_vars))
+  
+  g <- dat_in %>%
+    ggplot(aes(x = date, y = value_trend_std, col = name)) +
+    geom_line(lwd = 0.6) +
+    facet_wrap(. ~ id) +
+    scale_x_date(breaks = scale_date_breaks, date_labels = "%d %b") +
+    scale_color_brewer(palette = "Set1") +
+    labs(x = "Date", y = "Value (std)",
+         col = "Data") +
+    theme_bw() +
+    theme(legend.position = "top")
+  
+  if(plot_raw){
+    
+    g <- g + 
+      geom_line(data = dat_in, aes(y = value_std), alpha = 0.4)
+    
+  }
+  
+  return(g)
+  
+}
+
 
 # Plot forecasts (bars) ---------------------------------------------------
 
-observed <- raw_hosp %>%
-  dplyr::select(id, date, observed = all_adm)
-
-forecast <- adm_forecasts %>%
-  dplyr::select(forecast_from, model, id, date, horizon,
-                median = lower_0, lower_90, lower_50, upper_50, upper_90)
-
 plot_forecast_bars <- function(observed, forecast,
-                               trusts = NULL,
                                h = 7,
+                               trusts = NULL,
                                models = NULL,
                                facet_models = TRUE ,
-                               forecast_dates = NULL){
+                               use_dates = NULL){
   
   # Filter observed data
   observed_plt <- observed %>%
@@ -26,11 +65,12 @@ plot_forecast_bars <- function(observed, forecast,
                   horizon == h)
   if(!is.null(models)){
     forecast_plt <- forecast_plt %>%
-      dplyr::filter(model %in% models)
+      dplyr::filter(model %in% models) %>%
+      dplyr::mutate(model = ordered(model, models))
   }
-  if(!is.null(forecast_dates)){
+  if(!is.null(use_dates)){
     forecast_plt <- forecast_plt %>%
-      dplyr::filter(forecast_from %in% forecast_dates)
+      dplyr::filter(forecast_from %in% use_dates)
   }
   
   # Plot forecast data
@@ -44,14 +84,10 @@ plot_forecast_bars <- function(observed, forecast,
     geom_errorbar(data = forecast_plt,
                   aes(x = date, y = median, col = model, ymin = lower_50, ymax = upper_50),
                   alpha = 1, width = 0, lwd = 2) +
-    facet_grid(rows = vars(id), scales = "free_y") +
-    scale_x_date(limits = c(as.Date("2020-08-02"), as.Date("2021-02-14")),
-                 breaks = seq.Date(from = as.Date("2020-08-16"),
-                                   to = as.Date("2021-02-14"),
-                                   by = "4 weeks"),
-                 date_labels = "%d %b %y") +
+    facet_wrap(facets = vars(id), scales = "free_y") +
     scale_color_brewer(palette = "Set2") +
     labs(x = "Date", y = "Admissions",
+         title = paste0(h, "-days ahead forecasts"),
          col = "Forecast model") +
     theme_bw() +
     theme(legend.position = "top")
@@ -92,10 +128,14 @@ plot_forecast_ribbons <- function(observed, forecast,
   
   # Filter forecast data
   forecast_plt <- forecast %>%
-    dplyr::filter(id %in% trusts)
+    dplyr::filter(id %in% trusts) %>%
+    dplyr::mutate(forecast_from = as.character(forecast_from)) %>%
+    dplyr::ungroup()
   if(!is.null(models)){
     forecast_plt <- forecast_plt %>%
-      dplyr::filter(model %in% models)
+      dplyr::filter(model %in% models) %>%
+      dplyr::mutate(model = ordered(model, models),
+                    id = ordered(id, trusts))
   }
   if(!is.null(forecast_dates)){
     forecast_plt <- forecast_plt %>%
@@ -103,7 +143,7 @@ plot_forecast_ribbons <- function(observed, forecast,
   } else {
     all_dates <- unique(forecast_plt$forecast_from)
     forecast_plt <- forecast_plt %>%
-      dplyr::filter(forecast_from %in% all_dates[seq(1, length(all_dates), 2)])
+      dplyr::filter(forecast_from %in% all_dates[seq(1, length(all_dates), 1)])
   }
   
   # Plot forecast data
@@ -117,12 +157,8 @@ plot_forecast_ribbons <- function(observed, forecast,
     geom_ribbon(data = forecast_plt,
                 aes(x = date, y = median, fill = model, group = forecast_from, ymin = lower_50, ymax = upper_50),
                 alpha = 0.4) +
-    facet_grid(rows = vars(id), scales = "free_y") +
-    scale_x_date(limits = c(as.Date("2020-08-02"), as.Date("2021-02-14")),
-                 breaks = seq.Date(from = as.Date("2020-08-16"),
-                                   to = as.Date("2021-02-14"),
-                                   by = "4 weeks"),
-                 date_labels = "%d %b %y") +
+    facet_wrap(facets = vars(id), scales = "free_y") +
+    scale_y_continuous(limits = c(0, NA)) +
     scale_color_brewer(palette = "Set2") +
     scale_fill_brewer(palette = "Set2") +
     labs(x = "Date", y = "Admissions",
