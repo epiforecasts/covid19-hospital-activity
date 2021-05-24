6ee2928d45059a3f6f3a3af3f12bd95aa8fa1968
sophiemeakin <sophiemeakin@outlook.com>
2021-05-24 15:08:15 +0100

    Tidy baseline forecasting model

---
+++ R/run_baseline.R
@@ -0,0 +1,9 @@
+# Run forecast
+baseline_out <- forecast_baseline(data = dat, yvar = "all_adm", horizon = 14, samples = 1000,
+                                  train_from = forecast_date - 42, forecast_from = forecast_date)
+baseline_samples <- baseline_out$samples
+baseline_summary <- baseline_out$summary
+# Save forecast
+baseline_name <- paste0("admissions_snaive_trust_", forecast_date, ".rds")
+saveRDS(object = baseline_samples, file = here::here("data", "out", "admissions_forecast", "samples", baseline_name))
+saveRDS(object = baseline_summary, file = here::here("data", "out", "admissions_forecast", "summary", baseline_name))
