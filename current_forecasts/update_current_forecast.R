baf8553733a067316476f2756ecff3fb9056cf66
Sophie Meakin <sophiemeakin@outlook.com>
2022-01-11 10:04:36 +0000

    Admissions forecast 05 January

--- current_forecasts/update_current_forecast.R
+++ current_forecasts/update_current_forecast.R
@@ -11,7 +11,7 @@ source(here::here("R", "utils.R"))
 
 # Set up ------------------------------------------------------------------
 
-forecast_date <- as.Date("2021-12-22")
+forecast_date <- as.Date("2022-01-05")
 
 
 # Load and check case forecasts -------------------------------------------
